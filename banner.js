09ef9b51e5bd5a80c16d4b12a28fc2ecf8c60a40
Niaz Baharudeen <tuk05348@temple.edu>
2025-03-09 00:24:32 -0500

    Removed unnecessary script.

--- banner.js
+++
@@ -1,5 +0,0 @@
-window.onload = function setWidth(){
-    let sidebar = document.getElementById("sidebar")
-    var spacer = document.getElementById("spacer")
-    spacer.style.width = sidebar.clientWidth
-}
