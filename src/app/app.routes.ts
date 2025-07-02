fc257522d299c32708d8268a3971767d6697603d
Niaz Baharudeen <tuk05348@temple.edu>
2025-07-01 23:57:54 -0400

    Created home, about, projects, and contact components. Implemented routing for the navbar links.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'about',
+        component: AboutComponent,
+    },
+    {
+        path: 'projects',
+        component: ProjectsComponent,
+    },
+    {
+        path: 'contact',
+        component: ContactComponent,
+    },
+];
