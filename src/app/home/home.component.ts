3abae14c41fe84cecaf2ed1658b2be3773599b8a
Niaz Baharudeen <tuk05348@temple.edu>
2025-07-05 19:26:20 -0400

    Updated personal photo to be a square, moved icons out of home to put into future footer component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { EmailComponent } from '../icons/email/email.component';
-import { LinkedinComponent } from '../icons/linkedin/linkedin.component';
-import { GithubComponent } from '../icons/github/github.component';
 
 @Component({
   selector: 'app-home',
-  imports: [EmailComponent, LinkedinComponent, GithubComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
