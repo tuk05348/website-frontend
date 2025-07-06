2cb7481f3d6c395ed76ab90449b710c5ca67d9e7
Niaz Baharudeen <tuk05348@temple.edu>
2025-07-05 20:04:37 -0400

    Created a footer to hold icons and links at bottom of page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./Components/navbar/navbar.component";
+import { FooterComponent } from "./Components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
