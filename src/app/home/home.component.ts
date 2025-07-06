fea554988ea5b3684e28510345f6db16cc0cc147
Niaz Baharudeen <tuk05348@temple.edu>
2025-07-06 17:38:06 -0400

    Used the service in the home component to subscribe to the observable returned by the service and populate the bio span with the bio data from the api. Updated CSS to fix spacing in intro-container.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BioService } from '../Services/bio.service';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  constructor(private bioService: BioService) {}
+
+  bio: string = ''; 
+
+  ngOnInit() {
+    this.bioService.getBio().subscribe(
+      data => {
+        this.bio = data.bio;
+      }
+    )
+  }
 
 }
