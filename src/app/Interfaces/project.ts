98833eea9682334bd9bb0616ea88e0d1ebc46258
Niaz Baharudeen <tuk05348@temple.edu>
2025-07-09 00:47:48 -0400

    Created a project interface and a projects.json file to hold static projects data.

---
+++ src/app/Interfaces/project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    title: string;
+    image_url: string;
+    alt_text: string;
+    github_url: string;
+    year: number;
+    short_desc: string;
+    long_desc: string;
+}
