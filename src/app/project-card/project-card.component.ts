25c418d82166373daff4dc941f4bb6ed479f3362
Niaz Baharudeen <tuk05348@temple.edu>
2025-07-09 00:49:04 -0400

    Updated project card to take a project as input to populate itself. Update project page to import project json file and create projects array to populate project cards,

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { GithubComponent } from '../icons/github/github.component';
+import { Project } from '../Interfaces/project';
 
 @Component({
   selector: 'app-project-card',
@@ -8,5 +9,15 @@ import { GithubComponent } from '../icons/github/github.component';
   styleUrl: './project-card.component.css'
 })
 export class ProjectCardComponent {
+
+  @Input() project: Project = {
+    title: '',
+    image_url: '',
+    alt_text: '',
+    github_url: '',
+    year: 0,
+    short_desc: '',
+    long_desc: ''
+  };
 
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { ProjectCardComponent } from "../project-card/project-card.component";
+import projects_json from "../../../public/projects.json"
+import { Project } from '../Interfaces/project';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
-  imports: [ProjectCardComponent],
+  imports: [ProjectCardComponent, NgFor],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
-
+  projects: Project[] = projects_json as Project[]
 }
