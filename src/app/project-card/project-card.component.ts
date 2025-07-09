eae690ae0e798f78a8476496747fd834421cbf13
Niaz Baharudeen <tuk05348@temple.edu>
2025-07-08 23:49:58 -0400

    Created a basic project card with dummy data

---
+++ src/app/project-card/project-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { GithubComponent } from '../icons/github/github.component';
+
+@Component({
+  selector: 'app-project-card',
+  imports: [GithubComponent],
+  templateUrl: './project-card.component.html',
+  styleUrl: './project-card.component.css'
+})
+export class ProjectCardComponent {
+
+}
