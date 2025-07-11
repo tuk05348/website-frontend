2d156a1bd5b7d82f7bf8203c5c992f350498c841
Niaz Baharudeen <tuk05348@temple.edu>
2025-07-10 23:39:27 -0400

    Made a basic reactive form with validators to disable the submit button when a field's user input does not meet requirements.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required, Validators.maxLength(200)]) 
+  })
+
+  onSubmit() {
+    console.log(this.contactForm.value)
+  }
 
 }
