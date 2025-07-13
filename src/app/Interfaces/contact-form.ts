f5e5aa68ed5c101fc6fc54cd97a44d8f329e946b
Niaz Baharudeen <tuk05348@temple.edu>
2025-07-13 00:35:49 -0400

    Simplified the form, removed service and interface, followed demo, got working form.

--- src/app/Interfaces/contact-form.ts
+++
@@ -1,5 +0,0 @@
-export interface ContactForm {
-    name: string;
-    email: string;
-    message: string;
-}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,11 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import emailjs, {type EmailJSResponseStatus } from '@emailjs/browser';
+
+const serviceId: string = 'contact_service';
+const templateId: string = 'email_template';
+const myPublicKey: string = 'y8gA-9cIn9qNDtzid';
 
 @Component({
   selector: 'app-contact',
@@ -11,19 +16,14 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 export class ContactComponent {
 
   formSubmitted: boolean = false; //member to track whether form has been successfully submitted
+  formSuccess: boolean = false;
+  formError: boolean = false;
 
   contactForm = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
     message: new FormControl('', [Validators.required, Validators.maxLength(200)]) 
   })
-
-  onSubmit() {
-    if(this.contactForm.valid) {
-      console.log(this.contactForm.value);
-      this.formSubmitted = true;
-    }
-  }
 
   //getters to get form controls from the form group
   get name() {
@@ -36,6 +36,24 @@ export class ContactComponent {
 
   get message() {
     return this.contactForm.get('message');
+  }
+
+  onSubmit(e: Event) {
+    if(this.contactForm.valid) {
+      emailjs.sendForm(serviceId, templateId, e.target as HTMLFormElement, {
+        publicKey: myPublicKey,
+      })
+      .then(
+        () => {
+          console.log('Success!');
+          this.formSuccess = true;
+        },
+        (error) => {
+          console.log('Failed ...', (error as EmailJSResponseStatus).text);
+          this.formError = true;
+        },
+      );
+    }
   }
 
 }
